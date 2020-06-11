bd1de5da24dba1546317b68397ac6d7518e1125c
Duke Ko <dukhyunko93@gmail.com>
2020-06-11 13:43:41 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,14 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  if array.class == String
-    array.count {|empty_str| empty_str.empty?}
+total = 0
+index = 0
+  while index < array.length do
+    if array[index].class == String
+      array.count {|empty_str| empty_str.empty?}
+      total += 1
+    end
+    index + = 1
   end
+  total
 end
